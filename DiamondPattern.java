1f96b57dc9a59437e8ce13f5cc54dd2ccea23ef0
darshanborade07 <darshanborade67@gmail.com>
2024-05-25 00:38:34 +0530

    updated

---
+++ DiamondPattern.java
@@ -0,0 +1,36 @@
+package star_patterns;
+
+public class DiamondPattern {
+
+	public static void main(String[] args) {
+		int rows = 10;
+		for (int i = 1; i <= rows; i++) {
+			for (int j = rows; j > i; j--) {
+				System.out.print(" ");
+			}
+			System.out.print("*");
+			for (int k = 1; k < 2 * (i - 1); k++) {
+				System.out.print(" ");
+			}
+			if (i == 1) {
+				System.out.println("");
+			} else {
+				System.out.println("*");
+			}
+		}
+		for (int i = rows - 1; i >= 1; i--) {
+			for (int j = rows; j > i; j--) {
+				System.out.print(" ");
+			}
+			System.out.print("*");
+			for (int k = 1; k < 2 * (i - 1); k++) {
+				System.out.print(" ");
+			}
+			if (i == 1)
+				System.out.println("");
+			else
+				System.out.println("*");
+		}
+	}
+
+}
